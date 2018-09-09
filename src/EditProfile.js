15ded6eb1beefd90d6b3aad483aa82b10049d3ff
KASHAN HAIDER <contact@kashan-haider.com>
2018-09-09 20:57:16 +0500

    update

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoonLoader from 'react-spinners/MoonLoader';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -19,6 +22,8 @@ import CheckIcon from '@material-ui/icons/Check';
 import Edit from '@material-ui/icons/Edit';
 
 import * as firebase from 'firebase';
+import { Divider } from '../node_modules/@material-ui/core';
+import CommunicationStayPrimaryLandscape from 'material-ui/SvgIcon';
 function Transition(props) {
     return <Slide direction="up" {...props} />;
   }
@@ -30,25 +35,126 @@ class ProfileEdit extends Component {
   this.state ={
       coverIMG:'https://raw.githubusercontent.com/SMKH-PRO/ReactSocial/master/src/loadingcover.gif',
     ProfileIMG:'https://raw.githubusercontent.com/SMKH-PRO/ReactSocial/master/src/loadingprofile.gif',
+
     loading1:true,
     success1:false,
+    
+    loading3:false,
+    success3:false,
+
     loading2:true,
     success2:false,
+
     openDialog:false,
     DialogTitle: '',
     DialogDescription:'',
     OldCover:'',
     OldProfile:'',
+    FName: '',
+    Name:'',
+    LName:'',
+    DisableNameChange:true,
+    Bio:'',
+    BioError:false,
+    BioLessThan50:false,
+    Age:'',
+    City:'',
+    Country:'',
+    Gender:'',
+    IP:'',
+   MainLoading:true,
     
   }
   
   this.ChangeCover = this.ChangeCover.bind(this)
-  
+  this.RunBeforeRender = this.RunBeforeRender.bind(this)
+  this.SetData = this.SetData.bind(this)
+
   }
 
   componentDidMount(){
-    this.ChangeCover();
+    this.ChangeCover()
+   this.RunBeforeRender()
+
+
   }
+
+
+
+  RunBeforeRender(){
+    axios({
+        
+      url: 'https://www.kashan-haider.com/ReactSocial/ipinfo.php',
+      method:'GET',
+      headers:{'Content-Type':'application/x-www-form-urlencoded'},
+      
+  }).then((res)=>{
+    console.log(res.data)
+
+    
+    this.setState({IP:res.data.ip})
+  })
+  axios({
+        
+    url: 'https://geoip-db.com/json/',
+    method:'GET',
+    headers:{'Content-Type':'application/x-www-form-urlencoded'},
+    
+}).then((res)=>{
+
+  console.log(res)
+
+  this.setState({Country:  res.data.country_name, City: res.data.city  })
+
+  
+ 
+})
+
+
+
+
+    if(firebase.auth().currentUser != null){
+
+    let username = firebase.auth().currentUser.displayName
+
+
+    if(username != null ){
+this.setState({Name: username})
+      firebase.database().ref(`USERDETAILS/${firebase.auth().currentUser.uid}/Name`).set(this.state.Name)
+      this.setState({DisableNameChange:true})
+    }else{
+      
+      this.setState({DisableNameChange:false})
+    }
+
+
+    firebase.database().ref(`USERDETAILS/${firebase.auth().currentUser.uid}/`).once('value',(data)=>{
+
+if(data.hasChild("Age")){
+  this.setState({Age:data.child("Age").val()})
+}
+if(data.hasChild("Bio")){
+  this.setState({Bio:data.child("Bio").val()})
+}
+if(data.hasChild("LivesIn")){
+  this.setState({LivesIn:data.child("LivesIn").val()})
+}
+if(data.hasChild("Gender")){
+  this.setState({Gender:data.child("Gender").val()})
+}
+    }).then(()=>{
+
+      this.setState({MainLoading:false})
+    })
+
+  }else{
+    setTimeout(() => {
+      this.RunBeforeRender()
+
+    }, 500);
+  }
+
+}
   ChangeCover(){
     if(localStorage.getItem("Cover") != null){
       this.setState({coverIMG: localStorage.getItem("Cover")})
@@ -415,13 +521,184 @@ else{console.log("No Files Were Selected!")}
 handleCloseDialog = () => {
     this.setState({ openDialog: false });
   };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
   
+
+  CheckBioLenght(){
+    setTimeout(() => {
+      
+  
+    if(this.state.Bio.length < 50 ){
+      this.setState({BioLessThan50:true,BioError:true})
+    }
+    else if(this.state.Bio.length > 50  ){
+      this.setState({BioLessThan50:false})
+      if(this.state.Bio.length < 200){
+this.setState({BioError:false})
+      }
+      else{
+        this.setState({BioError:true})
+
+      }
+
+    }
+    else if(this.state.Bio.length > 200 ){
+      this.setState({BioError:true})
+
+      console.log(this.state.Bio ,":", this.state.Bio.length)
+    }
+   
+    else{
+      this.setState({BioError:false})
+    }  }, 10);
+
+
+
+  }
+
+
+
+  //; remove html tags
+  SetData(){
+    const { Age,Bio,LName,FName,LivesIn,Gender,BioError,BioLessThan50,City,Country,DisableNameChange } = this.state;
+
+
+
+  if(FName.length === 0  && DisableNameChange !== true){
+    this.setState({openDialog: true, DialogDescription:"You forgot to write First Name!, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+  else if(FName.length > 10  && DisableNameChange !== true){
+    this.setState({openDialog: true, DialogDescription:"First Name Must Not Be Longer Than 10 Characters!" ,DialogTitle:'An Error Occurred!' });  
+  }
+  else if(FName.length < 3  && DisableNameChange !== true){
+    this.setState({openDialog: true, DialogDescription:"First Name Must Not Be Shorter Than 3 Characters!" ,DialogTitle:'An Error Occurred!' });  
+  }
+
+  else if(LName.length === 0  && DisableNameChange !== true ){
+    this.setState({openDialog: true, DialogDescription:"You forgot to write Last Name!, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+
+  else if(LName.length > 10  && DisableNameChange !== true){
+    this.setState({openDialog: true, DialogDescription:"Last Name Must Not Be Longer Than 10 Characters!" ,DialogTitle:'An Error Occurred!' });  
+  }
+
+  
+  else if(LName.length < 3  && DisableNameChange !== true){
+    this.setState({openDialog: true, DialogDescription:"Last Name Must Not Be Shorter Than 3 Characters!" ,DialogTitle:'An Error Occurred!' });  
+  }
+
+  else if(Bio.length === 0 ){
+    this.setState({openDialog: true, DialogDescription:"You forgot to write Bio !, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+  else if(Bio.length <50 ){
+    this.setState({openDialog: true, DialogDescription:"You must write ''Bio'' With Atleast 50 Characters" ,DialogTitle:'An Error Occurred!' });
+  }
+  
+  else if(Bio.length >200 ){
+    this.setState({openDialog: true, DialogDescription:"Bio Must Be Shorter Than 200 Characters !, Character Limit Exceeded! " ,DialogTitle:'An Error Occurred!' });
+  }
+  else if(Country.length === 0 ){
+    this.setState({openDialog: true, DialogDescription:"You forgot to write Country !, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+  else if(Country.length > 30){
+    this.setState({openDialog: true, DialogDescription:"Invalid Country Name, Character Limit Exceeded, \n If your country name has longer than 30 Characters, Please write shortname of your country For Example: Pakistan for 'Islamic Republic Of Pakistan' " ,DialogTitle:'An Error Occurred!' });  
+  }
+  else if(City.length === 0 ){
+    this.setState({openDialog: true, DialogDescription:"You forgot to write City !, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+  
+  else if(City.length > 30){
+    this.setState({openDialog: true, DialogDescription:"Invalid City Name, Character Limit Exceeded" ,DialogTitle:'An Error Occurred!' });  
+  }
+  else if(Age.length === 0 ){
+    this.setState({openDialog: true, DialogDescription:"You forgot to write Age !, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+  
+else if(Age > 60){
+  this.setState({openDialog: true, DialogDescription:"Age Must Not Be Greater Than 60, We Allow Users In The Age Of 13 to 60.!, If you doesnt comes between this age limit then you are not eligible to continue here.!" ,DialogTitle:'An Error Occurred!' });  
+}
+else if(Age < 13){
+  this.setState({openDialog: true, DialogDescription:"Age Must Not Be Smaller Than 13, We Allow Users In The Age Of 13 to 60.!, If you doesnt comes between this age limit then you are not eligible to continue here.!" ,DialogTitle:'An Error Occurred!' });  
+}
+
+
+  else if(Gender == "" ){
+    this.setState({openDialog: true, DialogDescription:"You forgot to Select Gender !, Please recheck the form before submitting!" ,DialogTitle:'An Error Occurred!' });
+  }
+
+  else{
+    this.setState({loading3:true,success3:false})
+
+
+ 
+    firebase.database().ref(`USERDETAILS/${firebase.auth().currentUser.uid}/IPDETAILS/${this.state.IP.replace(/\./g,'x')}`).set("Profile Edit")
+
+    firebase.database().ref(`USERDETAILS/${firebase.auth().currentUser.uid}`).set({
+     
+      Bio:Bio,
+      Country:Country,
+      City:City,
+      Age:Age,
+      Gender:Gender,
+      
+    }).then(()=>{
+
+if( DisableNameChange !== true){
+  firebase.database().ref(`USERDETAILS/${firebase.auth().currentUser.uid}/Name`).set({
+    Name: FName+" "+LName,
+    FName: FName,
+    LName:LName}).then(()=>{
+
+
+      firebase.auth().currentUser.updateProfile({
+ 
+        displayName:FName+" "+LName
+      }).then(()=> {
+        // Update successful.
+        this.setState({loading3:false,success3:true})
+
+      }).catch((err)=> {
+        // An error happened.
+        this.setState({   
+          loading3:false,success3:false, openDialog: true, DialogDescription:err.toString(),DialogTitle:'An Error Occurred!' }); 
+      })
+    })
+      ;}
+      else{
+        this.setState({loading3:false,success3:true})
+
+      }
+    }).catch((err)=>{
+
+      this.setState({   
+        loading3:false,success3:false, openDialog: true, DialogDescription:err.toString(),DialogTitle:'An Error Occurred!' }); 
+    })
+  
+
+
+    
+
+
+  }
+  
+  
+ 
+
+
+  }
   render() {
-    const { loading1, success1,loading2, success2 } = this.state;
+    const { loading1,loading3,success3, success1,loading2, success2,Age,Bio,Name,LivesIn,Gender,BioError,BioLessThan50,City,Country } = this.state;
 
     
     return (
       <div>
+           <div class="center-div">
+            <MoonLoader  sizeUnit={"px"} className="ProfileImageLoader" color="#2196f3" size={80}  loading={this.state.MainLoading} /></div>
 
 
 
@@ -472,7 +749,7 @@ handleCloseDialog = () => {
  <input onChange={this.handleButtonChange.bind(this)} style={{display:'none'}} className="UploadCoverBtn" accept="image/*" id="cover-file"  type="file" />
       <label htmlFor="cover-file">
     <Button  className="UploadCoverBtn" component="span"  variant="contained" color="secondary"  disabled={loading1} >             {success1 ? (<span><span>Cover Changed</span> <CheckIcon style={{maxHeight:10}}/></span> ): 'Change Cover Image'}
- </Button>{loading1 && <CircularProgress style={{position:'absolute',right:100,marginTop:'15px'}} className="UploadCoverBtn" size={24}  />}</label>  
+ </Button>{loading1 && <CircularProgress style={{marginRight:100,marginTop:'15px'}} className="UploadCoverBtn" size={24}  />}</label>  
      </div>
   <BPImage style={{ display: 'block'}} wow="Cover" className="BPCover" src={this.state.coverIMG} caption="Cover Picture Of This User" >
  
@@ -502,21 +779,93 @@ handleCloseDialog = () => {
  </div>
  {loading2 && <CircularProgress className="LoadingNewProfile" size={68}  />}
         <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            Lizard
+          <Typography gutterBottom  component="span">
+  
+  
+  
+  { this.state.DisableNameChange ?(
+    <TextField id="FullName" InputProps={{ readOnly: true, }} label="Name"  placeholder="FullName" value={this.state.Name} margin="normal" />
+  ): (
+    <span>
+  <TextField id="FNAME" label="First Name" placeholder="Cannot be changed later!" value={this.state.FName} onChange={this.handleChange('FName')} margin="normal" />
+
+  <TextField id="LNAME" label="Last Name" placeholder="Cannot be changed later!" value={this.state.LName} onChange={this.handleChange('LName')} margin="normal" />
+  {this.state.DisableNameChange ? '':<p style={{color:'red'}}>Name Cannot Be Changed Later!, Make Sure To Write Valid Name!.</p>}
+  </span>
+  
+    )}
+        
           </Typography>
-          <Typography component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+          <Typography component="span">
+         
+          <TextField
+          error={BioError}
+          style={{width:'100%'}}
+          id="textarea"
+          label="Bio "
+          rows="3"
+          onPaste={this.CheckBioLenght.bind(this)}
+          onKeyDown={this.CheckBioLenght.bind(this)}
+          placeholder="Short Description About Your Self With Minimum 50 Characters And Maximum 200 Characters!."
+          multiline
+          onChange={this.handleChange('Bio')}
+          value={Bio}
+          disable={this.state.MainLoading}
+          margin="normal"
+        />
+
+
+       
+       {BioLessThan50 ? ( <span> {BioError ? (<p style={{color:'red',float:'right'}}>{Bio.length} / 50 </p>):(<p style={{color:'green',float:'right'}}>{Bio.length} / 50 </p>)}</span>):
+      
+      (
+        ( <span> {BioError ? (<p style={{color:'red',float:'right'}}>{Bio.length} / 200 </p>):(<p style={{color:'green',float:'right'}}>{Bio.length} / 200 </p>)}</span>)
+      )}<br/>
           </Typography>
+          <br/><br/><br/>
+          <p style={{fontWeight:600}}>About </p>
+          <Divider/>
+          <TextField id="City"           disable={this.state.MainLoading}
+ label="Country"           disable={this.state.MainLoading}
+ placeholder="Country Where You Live!" value={Country} onChange={this.handleChange('Country')} margin="normal" />
+ <TextField            disable={this.state.MainLoading}
+ id="City" label="City" placeholder="City Where You Live!" value={City} onChange={this.handleChange('City')} margin="normal" />
+<br/>
+<TextField id="Age" label="Age"              disable={this.state.MainLoading}
+        type="number"
+ placeholder="i.e: 18" value={Age} onChange={this.handleChange('Age')} margin="normal" /><br/>
+ 
+ <TextField
+           disable={this.state.MainLoading}
+
+          id="select-gender"
+          select
+          label="Select"
+          value={this.state.Gender}
+          onChange={this.handleChange('Gender')}
+        
+          helperText="Select Your Gender "
+          margin="normal"
+        >
+            <MenuItem  value="Male">
+              Male
+            </MenuItem>
+            <MenuItem value="Female">
+              Female
+            </MenuItem>
+        </TextField>
+
+
+
         </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+      <CardActions style={{float:'right'}}>
+      <span>
+     <Button   className="SaveChanges" onClick={this.SetData}  variant="contained" color="primary"  disabled={loading3} >             {success3 ? (<span><span>Changes Saved</span> <CheckIcon style={{maxHeight:10}}/></span> ): 'Save Changes'}
+ </Button>{loading3 && <CircularProgress  size={24}  />}
+ </span>
+
+
+
       </CardActions>
     </Card>   
 
